0eaf93a9bd36ba9569f7f2ca4806c4f45d404c31
TheBoringDude <iamcoderx@gmail.com>
2021-04-22 10:19:25 +0800

    render each blog in blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -3,18 +3,23 @@ import { BaseLayout } from '@layouts/base';
 import { Container } from '@components/container';
 import { usePupsColor } from '@lib/theme';
 import { ThemeLinkButton } from '@components/theme/buttons';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { getAllBlogs } from '@lib/blog/blogs';
+import { BlogDataProps } from '@lib/blog/types';
 
-const SampleBlogContent = {
-  title: 'Hello World',
-  excerpt:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate reprehenderit similique eveniet dolores itaque fuga architecto aliquam velit est ratione.',
-  date: new Date().toUTCString(),
-  author: 'Adrich Laceste'
+type AllBlogStaticProps = {
+  blogs: BlogDataProps[];
 };
 
-const blog = SampleBlogContent;
+export const getStaticProps: GetStaticProps = async (context) => {
+  const allBlogs = getAllBlogs();
 
-const BlogPage = () => {
+  return {
+    props: { blogs: allBlogs }
+  };
+};
+
+const BlogPage = ({ blogs }: AllBlogStaticProps) => {
   const pupmode = usePupsColor();
 
   return (
@@ -24,26 +29,27 @@ const BlogPage = () => {
           <h2 className="text-4xl font-black text-white tracking-widde">Crypto Blogs!</h2>
 
           <section className="my-8 w-2/3 mx-auto">
-            <div className="bg-coolGray-800 rounded-md">
-              <h3 className={`${pupmode.color} text-2xl text-white py-2 pl-4 rounded-t-md`}>
-                {blog.title}
-              </h3>
-              <div className="px-6 my-3">
-                <p className="text-gray-300 text-lg py-2">{blog.excerpt}</p>
-                <div className="mt-2 mb-6">
-                  <ThemeLinkButton
-                    href="/blog/hello-world"
-                    className="py-2 px-6 rounded-lg text-white"
-                  >
-                    Read More
-                  </ThemeLinkButton>
+            {blogs.map((blog, index) => (
+              <div key={index} className="bg-coolGray-800 rounded-md">
+                <h3 className={`${pupmode.color} text-2xl text-white py-2 pl-4 rounded-t-md`}>
+                  {blog.title}
+                </h3>
+                <div className="px-6 my-3">
+                  <p className="text-gray-300 text-lg py-2">{blog.excerpt}</p>
+                  <div className="mt-2 mb-6">
+                    <ThemeLinkButton
+                      href={`/blog/${blog.slug}`}
+                      className="py-2 px-6 rounded-lg text-white"
+                    >
+                      Read More
+                    </ThemeLinkButton>
+                  </div>
+                </div>
+                <div className="py-2 px-4 bg-coolGray-700 rounded-b-md text-right text-sm">
+                  <p className="text-gray-400">{new Date(blog.date).toUTCString()}</p>
                 </div>
               </div>
-              <div className="py-2 px-4 flex items-center justify-between bg-coolGray-700 rounded-b-md">
-                <h4 className="text-gray-200 hover:underline">@{blog.author}</h4>
-                <p className="text-gray-400">{blog.date}</p>
-              </div>
-            </div>
+            ))}
           </section>
         </div>
       </Container>
