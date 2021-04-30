54e45aa425072bc4b5b64631d60952e1002e9963
TheBoringDude <iamcoderx@gmail.com>
2021-04-30 20:50:35 +0800

    make logo image clickable to home

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,6 +3,7 @@
 */
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { Container } from './container';
 import { ToggleThemeButton } from './toggle-theme';
 
@@ -10,7 +11,11 @@ export const Navigation = () => {
   return (
     <Container>
       <nav className="py-4 flex items-center justify-between">
-        <Image src="/logo-min.png" height={35} width={215} />
+        <Link href="/">
+          <a>
+            <Image src="/logo-min.png" height={35} width={215} />
+          </a>
+        </Link>
 
         <ul className="inline-flex items-center text-coolGray-700 dark:text-white text-lg">
           <li className="ml-8">
