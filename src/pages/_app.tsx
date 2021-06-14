800aa028c0ae0af011948614fb4bffe1e95f680c
TheBoringDude <iamcoderx@gmail.com>
2021-06-14 17:54:52 +0800

    seo tags -> move other tags from `DefaultLayout` -> `next-seo.config.js`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,26 +3,15 @@ import type { AppProps } from 'next/app';
 
 import { DefaultSeo } from 'next-seo';
 
+import SEO from '../next-seo.config';
+
 import PupsProvider from '@lib/pupsThemeContext';
 import '../styles/tailwind.css';
 
 export default function CryptoPups({ Component, pageProps }: AppProps) {
   return (
     <PupsProvider>
-      <DefaultSeo
-        titleTemplate="%s | The World of CryptoPups"
-        openGraph={{
-          type: 'website',
-          locale: 'en_PH',
-          url: 'https://www.cryptopups.cf/',
-          site_name: 'World of CryptoPups'
-        }}
-        twitter={{
-          handle: '@apppllleee_pie',
-          site: '@apppllleee_pie',
-          cardType: 'summary_large_image'
-        }}
-      />
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </PupsProvider>
   );
