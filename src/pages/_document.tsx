0cc10ae50940b694c5f7bbba2fca0605bb273819
TheBoringDude <iamcoderx@gmail.com>
2021-06-25 17:38:21 +0800

    new google font link import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link
             href="https://fonts.googleapis.com/css2?family=Baloo+Tammudu+2:wght@400;500;600;700;800&family=Nunito:wght@200;300;400;600;700;800;900&family=PT+Sans:wght@400;700&display=swap"
             rel="stylesheet"
